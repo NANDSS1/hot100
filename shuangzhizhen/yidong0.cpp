909f61bd55f0e606b1e69e80aa0ebd01677a5eac
NANDSS1 <1175172084@qq.com>
2024-01-05 08:27:32 +0800

    算法

---
+++ shuangzhizhen/yidong0.cpp
@@ -0,0 +1,38 @@
+/*给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+
+ 
+
+示例 1:
+
+输入: nums = [0,1,0,3,12]
+输出: [1,3,12,0,0]
+示例 2:
+
+输入: nums = [0]
+输出: [0]*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //快慢指针
+        int fast = 0;
+        int slow = 0;
+        int n = nums.size();
+        while (fast < n) {
+            //fast负责收集非0元素
+            if(nums[fast] != 0){
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            }else fast++;
+        }
+
+        for(int i = slow;i < nums.size();i++){
+            nums[i] = 0;
+        }
+    }
+};
