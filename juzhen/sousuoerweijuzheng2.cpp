043aaf3fe5f16fe944bfc600f94c75cc3fad788c
NANDSS1 <1175172084@qq.com>
2024-01-06 09:09:58 +0800

    算法

---
+++ juzhen/sousuoerweijuzheng2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //对每行进行二分查找
+        for(const auto& vec:matrix){
+            int left = 0;
+            int right = matrix.size()-1;
+            //左闭右闭的二分查找
+            while(left <= right){//这里是和右开不一样的，这里是闭区间
+                int mid = left + (right-left)/2;
+                if(vec[mid] == target) return true;
+                else if(vec[mid] < target) left = mid + 1;
+                else if(vec[mid] > target) right = mid -1;//这里是和右开不一样的，是取mid-1
+            }
+        }
+
+        return false;
+        
+    }
+};
