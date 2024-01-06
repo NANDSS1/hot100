456e5a1588379c8e667c11d7bcc8aeb2274a043a
NANDSS1 <1175172084@qq.com>
2024-01-06 08:46:15 +0800

    算法

--- juzhen/juzhenzhiling.cpp
+++ juzhen/juzhenzhiling.cpp
@@ -1,10 +1,27 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();//row
-        int n = matrix[0].size();//col
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int l = 0;
+        int r = matrix[0].size()-1;
+        int t = 0;
+        int b = matrix.size()-1;
+        int all = matrix.size() * matrix[0].size();//给元素总数计数
+        int k = 0;
+
+        vector<int> ans;
+
+        while(k < all){
+            for(int i = l;i <= r&&k < all;i++,k++) ans.push_back(matrix[t][i]);
+            t++;
+            for(int i = t;i <= b&&k < all;i++,k++) ans.push_back(matrix[i][r]);
+            r--;
+            for(int i = r;i >= l&&k < all;i--,k++) ans.push_back(matrix[b][i]);
+            b--;
+            for(int i = b;i >= t&&k < all;i--,k++) ans.push_back(matrix[i][l]);
+            l++;
+        }
         
+        return ans;
 
     }
 };
