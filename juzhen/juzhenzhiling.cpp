999278b2c3693ee0168e8cc20787aeed1b0a2db9
NANDSS1 <1175172084@qq.com>
2024-01-06 08:46:01 +0800

    岁案发

---
+++ juzhen/juzhenzhiling.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();//row
+        int n = matrix[0].size();//col
+        
+
+    }
+};
