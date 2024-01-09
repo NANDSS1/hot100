e803ecbaceb69889dc97a0d7c0a2d1921a789194
NANDSS1 <1175172084@qq.com>
2024-01-09 08:49:50 +0800

    算法

---
+++ tree/duichengerchashu.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(TreeNode* left,TreeNode* right){
+        if(left == nullptr && right != nullptr) return false;
+        else if(left != nullptr && right == nullptr) return false;
+        else if(left == nullptr && right == nullptr) return true;//到底了
+        else if(left->val != right->val) return false;
+        
+        bool inside = isValid(left->right,right->left);
+        bool outside = isValid(left->left, right->right);
+
+        return inside&&outside;
+    }
+    bool isSymmetric(TreeNode* root) {
+        //从上到下遍历
+        if(root == nullptr) return false;
+
+        return isValid(root->left, root->right);
+
+    }
+};
