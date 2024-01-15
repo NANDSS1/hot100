245d442481a67fff69dfd37b0150e45d5519deb1
NANDSS1 <1175172084@qq.com>
2024-01-15 09:14:37 +0800

    算法

---
+++ tree/congqianxuhezhongxubianligouzaoerchashu.cpp
@@ -0,0 +1,73 @@
+/*class Solution {
+private:
+    unordered_map<int, int> index;//值去找结点
+
+public:
+    TreeNode* myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {//递归的终止条件，分治的
+            return nullptr;
+        }
+        
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;//前序遍历负责知道谁是根节点
+        // 在中序遍历中定位根节点
+        int inorder_root = index[preorder[preorder_root]];//中序遍历由根节点划分左右
+        
+        // 先把根节点建立出来
+        TreeNode* root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root->left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root->right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        // 构造哈希映射，帮助我们快速定位根节点
+        for (int i = 0; i < n; ++i) {
+            index[inorder[i]] = i;
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+};*/
+class Solution {
+public:
+    //建立一个hash表，由前序遍历节点的值快速去搜索中序遍历结点的位置
+    unordered_map<int,int> hashmap;
+    void myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right){
+        if (preorder_left > preorder_right) {//递归的终止条件，分治的
+            return nullptr;
+        }
+        
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;//前序遍历负责知道谁是根节点
+        // 在中序遍历中定位根节点
+        int inorder_root = index[preorder[preorder_root]];//中序遍历由根节点划分左右
+        
+        // 先把根节点建立出来
+        TreeNode* root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root->left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root->right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        for(int i = 0; i < preorder.size();i++){
+            hashmap[inorder[i]] = i;
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+
+    }
+};
+
